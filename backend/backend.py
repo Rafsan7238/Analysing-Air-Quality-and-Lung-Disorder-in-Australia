5f0017c21dd89564e8aa1c9c5848bdc958497d46
Petr Andreev <peter.andreev.russia@gmail.com>
2024-05-11 15:33:17 +1000

    backend functisons

--- backend/backend.py
+++ backend/backend.py
@@ -40,3 +40,62 @@ def create_indexes_endpoint():
         return json.dumps(results)
     except Exception as e:
         return json.dumps(e)
+
+
+### ^^ insert x endpoint()
+
+###############################
+# air quality vs lung disease 
+
+def get_air_quality_hourly_avg():
+    # get 2022_All_sites_air_quality_hourly_avg, 
+    # select parameter_names = ['CO', 'PM10', 'PM2.5', 'O3', 'SO2']
+    try: 
+        es = get_client()
+        bulker = get_bulker()
+
+
+    except Exception as e:
+        return json.dumps(e)
+    
+
+    pass
+
+
+def get_lung_cancer_join():
+    # get all lung cancer data aihw_cimar_mortality_persons_gccsa_2009
+    # merge with census_by_cob_data abs_2021census_g21a_aust_gccsa
+    # join on inner, ['gccsa_code', 'gccsa_name']
+    pass 
+
+def get_gender_lung_cancer():
+    # get male lung cancer 
+    # get female lung cancer
+    # join on gccsa_name
+    # columns 'gccsa_name', 'Lung cancer rate per 100k'
+    # add suffix (male, female)
+    pass
+
+
+def get_census_by_inc_emp():
+    # return census data 
+    pass
+
+#############################
+# weather vs sentiment 
+
+def get_historic_tweet_sentiment():
+    # return hiostoric tweet sentiment
+    pass 
+
+def get_rainfall_data():
+    # get all cities, 
+    # group by month
+    # get mean
+    pass 
+
+def get_temperature_data():
+    # get all cities, 
+    # group by month
+    # get mean
+    pass 
