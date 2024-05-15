5bdc6edd0a24ca925cbeef033bfd36a64369f698
William Chen <kiwitwwilliam@gmail.com>
2024-05-15 17:05:58 +1000

    format elastic client ipynb with markdowns. Move harvester index creation. mild refactor now that es has a larger timeotu

--- backend/backend.py
+++ backend/backend.py
@@ -1,14 +1,18 @@
 import json
 from flask import request
+from constants import *
+from elastic_client_provider import get_bulker, get_client
+
+from index_creation.create_bom_index import create_bom_index
+from index_creation.create_mastodon_index import create_mastodon_index
 from index_creation.create_mortality_index import create_mortality_index
 from index_creation.create_temperature_index import create_temperature_index
 from index_creation.create_air_quality_hourly_avg import create_air_quality_hourly_average
 from index_creation.create_census_g21b import create_census_g21b
 from index_creation.create_rainfall_index import create_rainfall_index
-from constants import *
-from elastic_client_provider import get_bulker, get_client
 from index_creation.create_asthma_by_region_index import create_asthma_by_region_index
 from index_creation.create_historic_tweets_index import create_historic_tweets_index
+
 import ingestion.historic_tweet_sentiments 
 import ingestion.asthma_by_region 
 import ingestion.air_quality_hourly_avg 
@@ -85,31 +89,36 @@ def insert_indexes():
 
 def create_indexes_endpoint():
     try:    
-        results = dict()
-        results[AIR_QUALITY_HOURLY_AVG] = create_air_quality_hourly_average(get_client())   
-        results[ASTHMA_BY_REGION_INDEX_NAME] = create_asthma_by_region_index(get_client())   
-        results[CENSUS_G21B] = create_census_g21b(get_client())   
-        results[HIST_TWEET_INDEX_NAME] = create_historic_tweets_index(get_client())
-        results[MORTALITY_FEMALES] = create_mortality_index(get_client(), MORTALITY_FEMALES)   
-        results[MORTALITY_MALES] = create_mortality_index(get_client(), MORTALITY_MALES)   
-        results[MORTALITY_PERSONS] = create_mortality_index(get_client(), MORTALITY_PERSONS)   
-        results[RAINFALL_ADELAIDE] = create_rainfall_index(get_client(), RAINFALL_ADELAIDE)   
-        results[RAINFALL_BRISBANE] = create_rainfall_index(get_client(), RAINFALL_BRISBANE)      
-        results[RAINFALL_CANBERRA] = create_rainfall_index(get_client(), RAINFALL_CANBERRA)   
-        results[RAINFALL_DARWIN] = create_rainfall_index(get_client(), RAINFALL_DARWIN)   
-        results[RAINFALL_MELBOURNE] =  create_rainfall_index(get_client(), RAINFALL_MELBOURNE)   
-        results[RAINFALL_PERTH] = create_rainfall_index(get_client(), RAINFALL_PERTH)   
-        results[RAINFALL_SYDNEY] = create_rainfall_index(get_client(), RAINFALL_SYDNEY)   
-        results[RAINFALL_TASMANIA] = create_rainfall_index(get_client(), RAINFALL_TASMANIA)   
+        es = get_client()
 
-        results[TEMPERATURE_ADELAIDE] = create_temperature_index(get_client(), TEMPERATURE_ADELAIDE)   
-        results[TEMPERATURE_BRISBANE] = create_temperature_index(get_client(), TEMPERATURE_BRISBANE)   
-        results[TEMPERATURE_CANBERRA] = create_temperature_index(get_client(), TEMPERATURE_CANBERRA)   
-        results[TEMPERATURE_DARWIN] = create_temperature_index(get_client(), TEMPERATURE_DARWIN)    
-        results[TEMPERATURE_MELBOURNE] = create_temperature_index(get_client(), TEMPERATURE_MELBOURNE)   
-        results[TEMPERATURE_PERTH] = create_temperature_index(get_client(), TEMPERATURE_PERTH)     
-        results[TEMPERATURE_SYDNEY] = create_temperature_index(get_client(), TEMPERATURE_SYDNEY)   
-        results[TEMPERATURE_TASMANIA] = create_temperature_index(get_client(), TEMPERATURE_TASMANIA)   
+        results = dict()
+        results[AIR_QUALITY_HOURLY_AVG] = create_air_quality_hourly_average(es)   
+        results[ASTHMA_BY_REGION_INDEX_NAME] = create_asthma_by_region_index(es)   
+        results[CENSUS_G21B] = create_census_g21b(es)   
+        results[HIST_TWEET_INDEX_NAME] = create_historic_tweets_index(es)
+        results[MORTALITY_FEMALES] = create_mortality_index(es, MORTALITY_FEMALES)   
+        results[MORTALITY_MALES] = create_mortality_index(es, MORTALITY_MALES)   
+        results[MORTALITY_PERSONS] = create_mortality_index(es, MORTALITY_PERSONS)   
+        results[RAINFALL_ADELAIDE] = create_rainfall_index(es, RAINFALL_ADELAIDE)   
+        results[RAINFALL_BRISBANE] = create_rainfall_index(es, RAINFALL_BRISBANE)      
+        results[RAINFALL_CANBERRA] = create_rainfall_index(es, RAINFALL_CANBERRA)   
+        results[RAINFALL_DARWIN] = create_rainfall_index(es, RAINFALL_DARWIN)   
+        results[RAINFALL_MELBOURNE] =  create_rainfall_index(es, RAINFALL_MELBOURNE)   
+        results[RAINFALL_PERTH] = create_rainfall_index(es, RAINFALL_PERTH)   
+        results[RAINFALL_SYDNEY] = create_rainfall_index(es, RAINFALL_SYDNEY)   
+        results[RAINFALL_TASMANIA] = create_rainfall_index(es, RAINFALL_TASMANIA)   
+
+        results[TEMPERATURE_ADELAIDE] = create_temperature_index(es, TEMPERATURE_ADELAIDE)   
+        results[TEMPERATURE_BRISBANE] = create_temperature_index(es, TEMPERATURE_BRISBANE)   
+        results[TEMPERATURE_CANBERRA] = create_temperature_index(es, TEMPERATURE_CANBERRA)   
+        results[TEMPERATURE_DARWIN] = create_temperature_index(es, TEMPERATURE_DARWIN)    
+        results[TEMPERATURE_MELBOURNE] = create_temperature_index(es, TEMPERATURE_MELBOURNE)   
+        results[TEMPERATURE_PERTH] = create_temperature_index(es, TEMPERATURE_PERTH)     
+        results[TEMPERATURE_SYDNEY] = create_temperature_index(es, TEMPERATURE_SYDNEY)   
+        results[TEMPERATURE_TASMANIA] = create_temperature_index(es, TEMPERATURE_TASMANIA)   
+
+        results[MASTODON] = create_mastodon_index(es)
+        results[BOM_OBSERVATIONS] = create_bom_index(es)
     
         return json.dumps(results), 201
     except Exception as e:
